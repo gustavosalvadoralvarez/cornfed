059766b8fb77b9aea4a50ff7ff8766a31b21cfc4
gustavosalvadoralvarez <gustavosalvadoralvarez@gmail.com>
2014-11-19 15:23:31 -0500

    fixed timestamp bug

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 
 module.exports = function cornfed(worker, events) {
 	// ops :: { eventName: report_schema }
-	var _post, schema;
-	Object.keys(events).forEach(function set_handler(evnt, i) {
-		try{   // want to be extremely careful not to throw uncaught errors 
-			schema = events[evnt]; // in main thread and bring whole page down
-			_post = function _post(event) {
-				try{
-					var prop, e = {}, evnt = evnt;
-					schema.forEach(function(prop){
-						var val = event[prop];
-						if (val && val.nodeType){
-							val = {
-								'tag': val.tagName,
-								'class': val.classList.toString(),
-								'id': val.id,
-								'value': val.nodeValue
-							}
-						}
-						e[prop] = val;
-					})
-					e['timestampt'] = new Date().getTime();
-					e['type'] = event.type;
-					worker.postMessage(e)
-				} catch(e){
-					console.log(e)
-				}
-			}
-			document.addEventListener(evnt, _post, true);
-		} catch(e){
-			console.log(e)
-		}
-		
+	var _post, schema, evnt;
+	for (evnt in events){
+		set_handler(evnt)
+	}
+	window.addEventListener('beforeunload', function (){
+		worker.postMessage('session_end')
 	})
+	function set_handler(evnt) {
+		var schema, _post;
+		schema = events[evnt] || []; 
+		_post = function _post(event) {
+				var prop, val, e = {}, evnt = evnt;
+				var props = schema.length;
+				while (--props){
+					val = event[prop];
+					if (val && val.nodeType){
+						val = {
+							'tag': val.tagName,
+							'class': val.classList.toString(),
+							'id': val.id,
+							'value': val.nodeValue
+						}
+					}
+					e[prop] = val;
+				}
+				e['timestamp'] = new Date().getTime();
+				e['type'] = event.type;
+				worker.postMessage(e)
+
+		}
+		document.addEventListener(evnt, _post, true);
+	}
 }
